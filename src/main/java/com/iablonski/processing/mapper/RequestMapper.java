4539cac88fa7c5ba6a6b4fe7531f3639248985db
Alexander_I <alex.iablonski@gmail.com>
2024-03-13 20:15:27 +0100

    Updated 006-insert-roles.yml

--- src/main/java/com/iablonski/processing/mapper/RequestMapper.java
+++ src/main/java/com/iablonski/processing/mapper/RequestMapper.java
@@ -3,9 +3,10 @@ package com.iablonski.processing.mapper;
 import com.iablonski.processing.domain.Request;
 import com.iablonski.processing.dto.RequestDetailsDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RequestMapper {
-
+    @Mapping(target = "phoneNumber", source = "phoneDetails.phone")
     RequestDetailsDTO toRequestDTO(Request request);
 }
